43e69f7111014aa452f5a10107bd82a076b8c7de
omercsx <omercagris@gmail.com>
2025-08-22 17:17:39 -0400

    exercise 4

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers';
 
-function Guess({ value }) {
-  return <p className="guess">
-    {range(5).map((index) => (
-      <span key={index} className="cell">
-        {value[index] || ''}
-      </span>
-    ))}
-  </p>;
+function Guess({ value, answer }) {
+  const result = checkGuess(value, answer);
+
+  return (
+    <p className="guess">
+      {range(5).map((index) => {
+        const className = result ? `cell ${result[index].status}` : 'cell';
+        return (
+          <span key={index} className={className}>
+            {result ? result[index].letter : ''}
+          </span>
+        );
+      })}
+    </p>
+  );
 }
 
 export default Guess;
