ff25268e3b766d7f5edcc82aae1b1262e65c864e
omercsx <omercagris@gmail.com>
2025-08-22 11:04:51 -0400

    Exercise 2

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import GuessInput from '../GuessInput/GuessInput';
+import GuessResults from '../GuessResults/GuessResults';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -10,9 +11,18 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>
-    <GuessInput />
-  </>;
+  const [guesses, setGuesses] = useState([]);
+
+  function handleSubmitGuess(guess) {
+    setGuesses([...guesses, guess]);
+  }
+
+  return (
+    <>
+      <GuessResults guesses={guesses} />
+      <GuessInput onSubmitGuess={handleSubmitGuess} />
+    </>
+  );
 }
 
 export default Game;
--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,18 +1,29 @@
 import React, { useState } from 'react';
 
-function GuessInput() {
+function GuessInput({ onSubmitGuess }) {
   const [guess, setGuess] = useState('');
 
   function handleSubmit(e) {
     e.preventDefault();
     console.log({ guess });
+    onSubmitGuess(guess);
     setGuess('');
   }
 
-  return <form onSubmit={handleSubmit}>
-    <label htmlFor="guess-input">Enter guess:</label>
-    <input id="guess-input" type="text" value={guess.toUpperCase()} onChange={(e) => setGuess(e.target.value)} pattern='[A-Z]{5}' minLength={5} maxLength={5} />
-  </form>;
+  return (
+    <form onSubmit={handleSubmit} className="guess-input-wrapper">
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={(e) => setGuess(e.target.value.toUpperCase())}
+        pattern="[A-Z]{5}"
+        minLength={5}
+        maxLength={5}
+      />
+    </form>
+  );
 }
 
 export default GuessInput;
